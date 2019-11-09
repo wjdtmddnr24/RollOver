4623a3144714699388fce2fbdf082f4c81c4e8c0
Hia <aofur1216@gmail.com>
2019-11-09 13:51:39 +0900

    실습실 컴퓨터 라우팅 설정

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var laboratoryRouter = require('./laboratory');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET home page : 실습실 목록 */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+/* POST 실습실 추가 */
+router.post("/", function (req, res) {
+
+});
+
+/* GET 실습실 컴퓨터 배치도 */
+router.get('/:laboratory/', function (req, res) {
+  res.end('laboratory ' + req.params.laboratory);
+});
+
+/* GET 실습실 컴퓨터 한 대 제보 */
+router.get('/:laboratory/:computer', function (req, res) {
+  res.end(`${req.params.laboratory}/${req.params.computer}`);
 });
 
 module.exports = router;
