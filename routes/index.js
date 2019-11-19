2a271bd2dbffebc7321b1d138a47d0a02d9a0c29
Hia <aofur1216@gmail.com>
2019-11-19 18:28:06 +0900

    GET 메소드 라우팅 구현

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var laboratory = require('../db/laboratory');
 
 /* GET home page : 실습실 목록 */
 router.get('/', function (req, res, next) {
-    res.render('index');
+    // 실습실 목록 가져오기
+    laboratory.findOne({}, function (err, data) {
+        if (err) throw err;
+        res.json(data);
+    });
 });
 
 /* POST 실습실 추가 */
@@ -12,8 +17,13 @@ router.post("/", function (req, res) {
 });
 
 /* GET 실습실 컴퓨터 배치도 */
-router.get('/:laboratory/', function (req, res) {
-    res.render('laboratory');
+router.get('/:laboratory/', function (req, res, next) {
+    // 실습실 컴퓨터 목록 가져오기 + 최근 제보 목록 가져오기
+    laboratory.findOne({_id: req.params._id}, function (err, data) {
+        if (err) throw err;
+        res.json(data);
+    })
+
 });
 
 /* POST 실습실 컴퓨터 추가 */
@@ -22,8 +32,12 @@ router.post('/:laboratory/', function (req, res) {
 });
 
 /* GET 실습실 컴퓨터 한 대 제보 */
-router.get('/:laboratory/:computer', function (req, res) {
-    res.render('computer');
+router.get('/:laboratory/:computer', function (req, res, next) {
+    // 실습실 컴퓨터 한 대의 제보 가져오기
+    laboratory.findOne({_id: req.params._id}, function (err, data) {
+        if (err) throw err;
+        res.json(data);
+    });
 });
 
 /* POST 실습실 컴퓨터 한 대 제보 추가 */
