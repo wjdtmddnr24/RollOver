0116a905f441316f9c4630ac72b513d88c82762c
Hia <aofur1216@gmail.com>
2019-11-19 19:23:02 +0900

    GET 메소드 라우팅 구현 오류 수정

--- routes/index.js
+++ routes/index.js
@@ -27,11 +27,10 @@ router.post("/", function (req, res) {
 /* GET 실습실 컴퓨터 배치도 */
 router.get('/:laboratory/', function (req, res, next) {
     // 실습실 컴퓨터 목록 가져오기 + 최근 제보 목록 가져오기
-    laboratory.findOne({_id: req.params._id}, function (err, data) {
+    laboratory.findOne({_id: req.params.laboratory}, function (err, data) {
         if (err) throw err;
         res.json(data);
-    })
-
+    });
 });
 
 /* POST 실습실 컴퓨터 추가 */
@@ -53,7 +52,7 @@ router.post('/:laboratory/', function (req, res) {
 /* GET 실습실 컴퓨터 한 대 제보 */
 router.get('/:laboratory/:computer', function (req, res, next) {
     // 실습실 컴퓨터 한 대의 제보 가져오기
-    laboratory.findOne({_id: req.params._id}, function (err, data) {
+    laboratory.findOne({_id: req.params.laboratory}, function (err, data) {
         if (err) throw err;
         res.json(data);
     });
